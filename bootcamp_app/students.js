ff46dd68dd1ff2c4db1b78771cd604f894622f9b
Daniel Nascimento <danielhn@gmail.com>
2020-08-20 00:22:19 +0000

    Update to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,18 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohort = process.argv[2];
-const limit = process.argv[3];
-// console.log(cohort);
-// console.log(limit);
-
-pool.query(`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+const text = `
 SELECT students.id AS student_id, students.name AS student_name, cohorts.name AS cohort_name
 FROM students
 LEFT JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(text, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.student_name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const values = [cohortName]
+const text = `
 SELECT teachers.name AS teacher_name, cohorts.name AS cohort_name, COUNT(*) AS total_assistances
 FROM assistance_requests
 JOIN students ON assistance_requests.student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
 JOIN teachers ON assistance_requests.teacher_id = teachers.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
-`)
+`;
+
+
+pool.query(text, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort_name}: ${user.teacher_name}`);
